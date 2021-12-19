1509489210c853e8867679fa93c5ba9697420b60
Dmitriy <setterdsa@gmail.com>
2021-12-19 22:14:01 +0300

    Add women

--- coolsite/urls.py
+++ coolsite/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from women.views import index
+from women.views import *
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('women/', index),
+    path('', include('women.urls')),
 ]
--- women/views.py
+++ women/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 def index(request): #HttpRequest
     return HttpResponse("Страница приложения women!")
 
+def categories(request): #HttpRequest
+    return HttpResponse("<h1>Статьи по категориям.</h1>")
+
+
 
 
 
